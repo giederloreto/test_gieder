76a8c0bb28ea58e0f1a0e0154b98c7fef190b43a
Gieder Loreto <ctl_gloreto@uolinc.com>
2023-02-20 13:30:23 -0300

    fix: change name app

--- lib/application.dart
+++ lib/application.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:test_gieder/view/home_view.dart';
 
-class GiederApp extends StatelessWidget {
-  const GiederApp({super.key});
+class GiederAppTest extends StatelessWidget {
+  const GiederAppTest({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:test_gieder/application.dart';
 
 void main() {
-  runApp(const GiederApp());
+  runApp(const GiederAppTest());
 }
